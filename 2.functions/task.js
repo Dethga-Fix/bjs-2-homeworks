cae8e92afd280adb684de2425a679edf59f8f27c
Kiselev Danila <kdanila03062006@gmail.com>
2026-02-08 16:57:59 +0300

    Домашнее задание к лекции 2 «Функции»

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,88 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (arr.length === 0) return { min: 0, max: 0, avg: 0 };
+  
+  let mn = arr[0];
+  let mx = arr[0];
+  let s = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < mn) mn = arr[i];
+    if (arr[i] > mx) mx = arr[i];
+    s += arr[i];
+  }
+  
+  const avg = Number((s / arr.length).toFixed(2));
+  
+  return { min: mn, max: mx, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  let s = 0;
+  for (let i = 0; i < arr.length; i++) {
+    s += arr[i];
+  }
+  return s;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  let mn = arr[0];
+  let mx = arr[0];
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < mn) mn = arr[i];
+    if (arr[i] > mx) mx = arr[i];
+  }
+  
+  return mx - mn;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  let sEven = 0;
+  let sOdd = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sEven += arr[i];
+    } else {
+      sOdd += arr[i];
+    }
+  }
+  
+  return sEven - sOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  let sEven = 0;
+  let cEven = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sEven += arr[i];
+      cEven++;
+    }
+  }
+  
+  if (cEven === 0) return 0;
+  
+  return sEven / cEven;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  let mxRes = -Infinity;
+  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const res = func(...arrOfArr[i]);
+    
+    if (res > mxRes) mxRes = res;
+  }
+  
+  return mxRes;
 }
